9cd1b61686a82bd56d5ef08bb8c8d2e332da1b51
GS Software Solutions <bsvenki@hotmail.com>
2018-04-16 09:34:53 +0530

    Name change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes,RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, MatProgressBarModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import { routing } from './app.routing';
+import 'hammerjs';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing, 
+    MatToolbarModule, 
+    MatButtonModule, 
+    MatIconModule, 
+    MatSidenavModule,
+    MatProgressBarModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
